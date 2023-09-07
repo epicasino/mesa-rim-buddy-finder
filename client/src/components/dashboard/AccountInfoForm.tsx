e11f58b1fbe79203cb5a00fd67d6a3155efafed8
Ray Badua <rayjonathanbadua@gmail.com>
2023-09-07 01:24:14 -0700

    added AccountInfoForm component for dashboard

---
+++ client/src/components/dashboard/AccountInfoForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AccountInfoForm() {
+  return (
+    <form className="xs:col-span-2 md:col-span-1 w-full h-full bg-slate-200 bg-opacity-75 rounded grid grid-rows-8 items-center">
+      <h5>Account Info</h5>
+    </form> 
+  );
+}
