fd638b3a13652e694a3acedead742b38b520ca91
epicasino2 <rayjonathanbadua@gmail.com>
2023-09-08 22:07:54 -0700

    merge and pop

---
+++ client/src/pages/Explore.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Explore() {
+  return (
+    <main>
+      <section className="bg-dashboard bg-cover min-h-screen flex flex-col items-center justify-center">
+        <div className="text-center bg-slate-100 bg-opacity-50 md:w-[98vw] min-h-[98vh] grid grid-rows-6 justify-items-center items-center gap-5 rounded p-5 m-5">
+          <h1>Hi</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
